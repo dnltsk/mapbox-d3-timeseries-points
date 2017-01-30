705f317ae96ddd9662700085f4390178b2ea2cc3
Daniel Teske <daniel.teske@meteogroup.com>
2017-01-30 23:42:07 +0100

    code format

--- app.js
+++ app.js
@@ -16,19 +16,26 @@ let projectLonLatToMapbox = (lonlat) => {
 
 let renderStations = () => {
   stations
-      .attr("cx", function(station, i){return projectLonLatToMapbox(station.coord)[0]})
-      .attr("cy", function(station, i){return projectLonLatToMapbox(station.coord)[1]})
-      .style("fill", function(station, i){return colorTable(station)})
-      .attr("r", 5);
+      .attr("cx", function (station, i) {
+        return projectLonLatToMapbox(station.coord)[0]
+      })
+      .attr("cy", function (station, i) {
+        return projectLonLatToMapbox(station.coord)[1]
+      })
+      .style("fill", colorTable)
+      .attr("r", 8);
 };
 
 let colorTable = (station) => {
-  switch(station.value){
-    case 0: return "green";
-    case 1: return "yellow";
-    case 2: return "red";
+  switch (station.value) {
+    case 0:
+      return "green";
+    case 1:
+      return "yellow";
+    case 2:
+      return "red";
     default:
-      throw Error("cannot find color for station "+ JSON.stringify(station));
+      throw Error("cannot find color for station " + JSON.stringify(station));
   }
 };
 
@@ -47,7 +54,7 @@ let stationData = [];
 for (let i = 0; i < 10000; i++) {
   stationData.push({
     coord: [Math.random() * 360 - 180, Math.random() * 180 - 90],
-    value: Math.floor(Math.random()*3)
+    value: Math.floor(Math.random() * 3)
   });
 }
 let stations = svg.append("g").classed("stations", true)
